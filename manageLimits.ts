7651f66ee3fbd094bbc8ca70cfe97ce5eef34e39
evgeniko <97796468+evgeniko@users.noreply.github.com>
2024-10-28 21:38:43 +0100

    Ntt sdk (#2)
    * use ntt solana sdk instead of relying on the idl
    
    * adjust sepolia inbound

--- manageLimits.ts
+++ manageLimits.ts
@@ -1,15 +1,19 @@
 import * as anchor from "@project-serum/anchor";
 import { Connection as solanaConnection, PublicKey, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
-import idl from "./idl.json";
 import * as multisig from "@sqds/multisig";
 const fs = require('fs');
 
+import "@wormhole-foundation/sdk-solana-ntt";
+import { getNttProgram, NTT } from "@wormhole-foundation/sdk-solana-ntt";
+
 (async () => {
-    const walletPath = "/WpyVik9YdWs8jnFoLnRBxfGfgQKgSxfEs5MYfTRwLCY.json";
+    // TODO: needs to be one of the signers of the Squad
+    const walletPath = "ww4AoktpBksE1M4zk6vWxujbqtaqhGY59VMuAxV4yxq.json";
     const walletJSON = JSON.parse(fs.readFileSync(walletPath, "utf-8"));
     const walletKeypair = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(walletJSON));
 
-    const programId = "nt5qvUo98jeiYSW8opSJt7F3e8XnSX1xXt4qKmCrvgd";
+    // TODO: change to your NTT manager address
+    const programId = "ntTPvQLdTaMUXNX1LBbAvbz9i2GyH3KwB8Q3NFDZhzK";
     const programIdKey = new PublicKey(programId);
 
     const solanaCon = new solanaConnection("https://api.devnet.solana.com");
@@ -19,7 +23,8 @@ const fs = require('fs');
         programIdKey
     );
 
-    const squadsAddress = new PublicKey("5xy4cJ7dWjZ1zqfePVCxGA8mgpsYW54G3Be4UHfmyVCF");
+    // TODO: change to your squads pubkey
+    const squadsAddress = new PublicKey("squri3LHQffRXoy3ZFbNCWg2Ck38jzcNXNQJQQJzPEa");
     const [vaultPda] = multisig.getVaultPda({
         multisigPda: squadsAddress,
         index: 0,
@@ -44,14 +49,20 @@ const fs = require('fs');
     const currentTransactionIndex = Number(multisigInfo.transactionIndex);
     const newTransactionIndex = BigInt(currentTransactionIndex + 1);
 
-    const program = new anchor.Program(idl as anchor.Idl, programId, provider);
+    const program = getNttProgram(
+        solanaCon,
+        programId,
+        "2.0.0" // ntt solana version parameter
+    );
+    
     const [outboundLimitPublicKey] = await PublicKey.findProgramAddress(
         [Buffer.from("outbox_rate_limit")],
         programIdKey
     );
-
+    // needs to have the correct amount of decimals for the respective chain
+    const outbountLimit = new anchor.BN(2150000000); // 2.150000000 tokens on Solana
     const outboundLimitInstruction = await program.methods
-        .setOutboundLimit({ limit: new anchor.BN(1000.005) })
+        .setOutboundLimit({ limit:  outbountLimit})
         .accounts({
             config: configPublicKey,
             owner: vaultPda,
@@ -65,29 +76,25 @@ const fs = require('fs');
         programIdKey
     );
     // List of ChainIds: https://github.com/wormhole-foundation/wormhole-sdk-ts/blob/fa4ba4bc349a7caada809f209090d79a3c5962fe/core/base/src/constants/chains.ts#L6
-    const inboundLimitInstruction = await program.methods
-        .setInboundLimit({ limit: new anchor.BN(1000.005), chain_id: 1 })
-        .accounts({
-            config: configPublicKey,
-            owner: vaultPda,
-            rateLimit: inboundrateLimitPublicKey,
-        })
-        .instruction();
-
-    const pauseInstruction = await program.methods
-    .setPaused(true)
-    .accounts({
-        config: configPublicKey,
+    const inboundLimit = new anchor.BN(1150000000); // 1.15
+    // will only work if the specific chain inbound limit was initialized (done with add-chain command)
+    const inboundLimitInstruction = await NTT.setInboundLimit(program as any, {
         owner: vaultPda,
-    })
-    .instruction();
+        chain: "Sepolia",
+        limit: new anchor.BN(inboundLimit.toString()),
+    });
+
+    const pauseInstruction = await NTT.createSetPausedInstruction(program as any, {
+        owner: vaultPda,
+        paused: false
+    });
 
     // Build a message with instructions we want to execute
     const testClaimMessage = new TransactionMessage({
         payerKey: vaultPda,
         recentBlockhash: (await solanaCon.getLatestBlockhash()).blockhash,
         //TODO: modify this based on the instruction you want to perform (outboundLimitInstruction / inboundLimitInstruction or pauseInstruction)
-        instructions: [pauseInstruction],
+        instructions: [inboundLimitInstruction],
     });
     
     const uploadTransactionIx = await multisig.instructions.vaultTransactionCreate({
@@ -106,11 +113,6 @@ const fs = require('fs');
       transactionIndex: newTransactionIndex,
       creator: squadMember.publicKey
     });
-
-    /*
-      ONLY DEVNET VERSION
-      transferOwnershipMainnet.ts can be used with Squads UI for Mainnet!!
-    */
 
     // proposalApprove method needs to be executed for every member of the squad!
     // only needed for testing purposes, if on devnet. 
--- transferOwnershipMainnet.ts
+++ transferOwnershipMainnet.ts
@@ -1,27 +1,29 @@
 import * as anchor from "@project-serum/anchor";
 import { Connection as solanaConnection, PublicKey, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
-import idl from "./idl.json"; 
 import * as multisig from "@sqds/multisig";
+import idl from "./idl.json"; 
 const fs = require('fs');
 
 (async () => {
-    const walletPath = "/WpyVik9YdWs8jnFoLnRBxfGfgQKgSxfEs5MYfTRwLCY.json";
-    const walletJSON = JSON.parse(fs.readFileSync(walletPath, "utf-8"));
-    const walletKeypair = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(walletJSON));
+ // TODO: needs to be token owner & creator of the Squads multisig
+ const tokenOwnerWalletPath = "WSjKF6e3bEuLKuQdgi6enzb3QAHAUZvs93dBGXRBUED.json";
+ const walletJSON = JSON.parse(fs.readFileSync(tokenOwnerWalletPath, "utf-8"));
+ const walletKeypair = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(walletJSON));
 
-    const programId = "nt5qvUo98jeiYSW8opSJt7F3e8XnSX1xXt4qKmCrvgd";
-    const programIdKey =  new PublicKey("nt5qvUo98jeiYSW8opSJt7F3e8XnSX1xXt4qKmCrvgd");
+ // TODO: change to your NTT manager address
+ const nttManagerProgramId = "Nt6LBJ3wXBrsCortqeALpYG3XdqpGaCd1GAhhrva4pg";
+ const nttManagerProgramIdKey =  new PublicKey(nttManagerProgramId);
 
-    const solanaCon = new solanaConnection("https://api.devnet.solana.com");
+ const solanaCon = new solanaConnection("https://api.devnet.solana.com");
 
-    const [configPublicKey, _configPublicKeyBump] = await PublicKey.findProgramAddress(
-        [Buffer.from("config")],
-        programIdKey
-      );
+ const [configPublicKey, _configPublicKeyBump] = await PublicKey.findProgramAddress(
+     [Buffer.from("config")],
+     nttManagerProgramIdKey
+   );
 
-    // claiming ownership from temporary account with squads sdk!
-    // TODO: change to your squads pubkey
-    const squadsAddress = new PublicKey("5xy4cJ7dWjZ1zqfePVCxGA8mgpsYW54G3Be4UHfmyVCF");
+ // claiming ownership from temporary account with squads sdk!
+ // TODO: change to your squads pubkey
+ const squadsAddress = new PublicKey("CnTS7RmoqVh88grwarBdkXM63avL4yaz8mtjzxjAj9zn");
     // Derive the PDA of the Squads Vault
     // this is going to be the Upgrade authority address, which is controlled by the Squad!
     const [vaultPda] = multisig.getVaultPda({
@@ -32,12 +34,12 @@ const fs = require('fs');
     // temporary pda, needed before claim instruction
     const [upgradeLockPublicKey, _upgradeLockPublicKey] = await PublicKey.findProgramAddress(
         [Buffer.from("upgrade_lock")],
-        programIdKey
+        nttManagerProgramIdKey
       );
     //   The programDataPublicKey is the PDA that stores the program's data
     const bpfLoaderUpgradeableProgramPublicKey = new PublicKey("BPFLoaderUpgradeab1e11111111111111111111111");
     const [programDataPublicKey, _programDataBump] = await PublicKey.findProgramAddress(
-    [programIdKey.toBuffer()],
+    [nttManagerProgramIdKey.toBuffer()],
     bpfLoaderUpgradeableProgramPublicKey 
     );
 
@@ -46,7 +48,7 @@ const fs = require('fs');
     const provider = new anchor.AnchorProvider(anchorConnection, wallet, { preflightCommitment: "confirmed" });
     anchor.setProvider(provider);
 
-    const program = new anchor.Program(idl as anchor.Idl, programId, provider);
+    const program = new anchor.Program(idl as anchor.Idl, nttManagerProgramId, provider);
     // delegate ownership to a temporary account!
     await program.methods
       .transferOwnership()
